2aa6bc3b84096862aee8e6342f657de23a45bd40
Lycio <Lycio@vip.qq.com>
2012-02-12 00:38:07 +0800

    补充鬼包翻译文件 Signed-off-by: Lycio <Lycio@vip.qq.com>

---
+++ lang/zh_CN/GhostPackage.lua
@@ -0,0 +1,81 @@
+return{
+	["ghost"] = "鬼包",
+
+	["guiguanyu"] = "鬼关羽",
+	["cv:guiguanyu"] = " ",
+	["designer:guiguanyu"] = "杀神附体小组",
+	["title:guiguanyu"] = "",
+	["code:guiguanyu"] = "若虚无",
+	
+	["wumo"] = "武魔",
+	[":wumo"] = "出牌阶段，打出或使用一张【杀】，可立即摸一张牌。",
+	["tuodao"] = "拖刀",
+	[":tuodao"] = "当任意角色对你使用【杀】被你闪避以后，可立即对你攻击范围内的任意一名角色使用一张【杀】。",
+
+	["guizhangfei"] = "鬼张飞",
+	["cv:guizhangfei"] = " ",
+	["designer:guizhangfei"] = "杀神附体小组",
+	["title:guizhangfei"] = "",
+	["code:guizhangfei"] = "若虚无",
+	
+	["longyin"] = "龙吟",
+	[":longyin"] = "你使用黑色的【杀】时无距离限制。",
+	["huxiao"] = "虎啸",
+	[":huxiao"] = "你使用红色的【杀】时可额外指定一个目标。",
+
+	["guilvbu"] = "鬼吕布",
+	["cv:guilvbu"] = " ",
+	["designer:guilvbu"] = "杀神附体小组",
+	["title:guilvbu"] = "",
+	["code:guilvbu"] = "若虚无",
+	
+	["sheji"] = "射戟",
+	[":sheji"] = "锁定技，出牌阶段，若你在目标角色的攻击范围内，则你对该角色使用的【杀】不可被闪避。",
+	["juelu"] = "绝戮",
+	[":juelu"] = "当打出的【杀】为最后一张手牌，攻击范围无限，你可以为这张【杀】指定至多两名目标角色。",
+
+	["guihuaxiong"] = "鬼华雄",
+	["cv:guihuaxiong"] = " ",
+	["designer:guihuaxiong"] = "杀神附体小组",
+	["title:guihuaxiong"] = "",
+	["code:guihuaxiong"] = "若虚无",
+	
+	["xiaoshou"] = "枭首",
+	[":xiaoshou"] = "你可以立即从对你造成伤害的来源处的装备区内获得一张牌，并可立即将它交给任意一名角色，或放置于任意一名角色的装备区内。",
+	["xiaoshou:obtain"] = "放入手牌",
+	["xiaoshou:equip"] = "装备此牌",
+
+	["guisimahui"] = "鬼司马徽",
+	["cv:guisimahui"] = " ",
+	["designer:guisimahui"] = "杀神附体小组",
+	["title:guisimahui"] = "",
+	["code:guisimahui"] = "若虚无",
+	
+	["shouyeghost"] = "授业",
+	[":shouyeghost"] = "出牌阶段，你可以主动弃掉一张手牌，令任意一名角色摸两张牌，每回合限一次。",
+	["jiehuoh"] = "解惑",
+	[":jiehuoh"] = "限定技，出牌阶段，可弃掉4张不同花色的手牌令自己上回合结束后死亡的一名角色复活后回复至三点体力，立即摸三张牌。",
+	["@disabuse"] = "解惑",
+
+	["shamoke"] = "沙摩柯",
+	["cv:shamoke"] = " ",
+	["designer:shamoke"] = "杀神附体小组",
+	["title:shamoke"] = "",
+	["code:shamoke"] = "若虚无",
+	
+	["qinwang"] = "勤王",
+	[":qinwang"] = "非自己回合，可主动替换自己以外任意一名角色承担伤害，每承担一点伤害，下一回合出牌阶段，你使用的【杀】或【决斗】（你为伤害来源时），造成的伤害+1。",
+	["#QinwangBuff"] = "%from 的【勤王】效果被触发，伤害从 %arg 点上升至 %arg2 点",
+	["@dutiful"] = "勤王",
+
+	["guizhuge"] = "鬼诸葛亮",
+	["cv:guizhuge"] = " ",
+	["designer:guizhuge"] = "杀神附体小组",
+	["title:guizhuge"] = "",
+	["code:guizhuge"] = "若虚无",
+	
+	["zhuangshen"] = "妆神",
+	[":zhuangshen"] = "回合开始阶段，你可以进行判定：若为黑色，则你立刻获得场上任意一名角色所有技能（不包括限定技）直至你的下回合开始阶段前。",
+	["qimen"] = "奇门",
+	[":qimen"] = "锁定技，始终不受延时锦囊牌影响。",
+}
